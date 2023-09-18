36753338c28b28a2baaa61883556cb03784b5b6c
SH0123 <sanghyoboy@naver.com>
2023-09-18 17:21:42 +0900

    set UITabBarController, UITableView

---
+++ NetflixClone/NetflixClone/Controllers/DownloadViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DownloadViewController.swift
+//  NetflixClone
+//
+//  Created by sanghyo on 2023/09/18.
+//
+
+import UIKit
+
+class DownloadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ NetflixClone/NetflixClone/Controllers/SearchViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchViewController.swift
+//  NetflixClone
+//
+//  Created by sanghyo on 2023/09/18.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBackground
+    }
+
+}
